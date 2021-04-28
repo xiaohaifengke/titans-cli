b62c6ae537db49a0c91afcfe3d2e37eb7683c1bb
Jhail <Jhail@foxmail.com>
2021-04-28 11:13:08 +0800

    download the templete by selected tag

--- utils/index.js
+++ utils/index.js
@@ -1,23 +1,27 @@
+const ora = require('ora')
+
 function sleep(interval) {
   return new Promise(resolve => setTimeout(resolve, interval))
 }
 
-async function wrapLoading(fn, message, options) {
+async function waitFnloading(fn, message, ...args) {
   const spinner = ora({
     text: message,
     color: 'cyan'
   }).start()
   
   try {
-    const res = await fn()
+    const res = await fn(...args)
     spinner.succeed()
     return res
   } catch (e) {
-    spinner.fail('request failed, refetch...')
+    spinner.fail('request failed, refetch again in five seconds')
+    await sleep(5000)
+    return waitFnloading(fn, message, ...args)
   }
 }
 
 module.exports = {
   sleep,
-  wrapLoading
+  waitFnloading
 }
